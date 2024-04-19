b208f66824e0e1c2f649e0463b18215348f42e7b
unknown <mthakore12@gmail.com>
2024-04-19 16:29:02 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+
+
+const IndexPage = () => (
+  
+    <div className=" font-bold text-red-900">
+      Mirage thakore
+    </div>
+  
+
+)
+
+
+export const Head = () => <Seo title="Home" />
+
+export default IndexPage
